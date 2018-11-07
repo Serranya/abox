696d6d0a7a8d2e7fbdacacc9947231afd75d2624
Peter Lamby <extern.plamby@novomind.com>
2018-11-07 16:06:14 +0100

    WIP

--- provision.sh
+++ provision.sh
@@ -18,7 +18,8 @@ pacman -Sy --needed --noconfirm htop \
                                 firefox-developer-edition \
                                 firefox-developer-edition-i18n-de \
                                 dex \
-                                keepassxc
+                                keepassxc \
+                                jre-openjdk
 
 pacman -S --noconfirm hugo
 ln -s /usr/share/applications/dropbox.desktop /etc/xdg/autostart/dropbox.desktop
@@ -31,6 +32,12 @@ localectl set-x11-keymap de
 timedatectl set-timezone Europe/Berlin
 
 useradd --create-home --shell /bin/bash serra
+cd /vagrant/user && find . -type f -exec install -v -D -o serra -g serra "{}" "/home/serra/{}" \;
+find /home/serra/.ssh/ -type f -exec chmod 600 "{}" \;
+find /home/serra/.ssh/ -type f -name \*.pub -exec chmod 644 "{}" \;
+chown serra: /home/serra/.ssh
+chmod 700 /home/serra/.ssh
+
 systemctl --user --global enable urxvtd.service
 #TODO WTF: works when _I_ execute it. Vagrant gives me: "error: clould not locate package 'exit'" ??!?!?
 #pacinstall --debug --noconfirm --remove virtualbox-guest-utils-nox --install virtualbox-guest-utils
